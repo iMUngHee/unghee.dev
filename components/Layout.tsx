15226293a2a9e47b7e2b017d392a3ac9c0f8ff17
LUH <ung7497@ajou.ac.kr>
2022-03-20 17:26:31 +0900

    [FIX] Get children on layout component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,10 +1,11 @@
 import { NextPage } from "next";
 import NavigationBar from "./NavigationBar";
 
-const Layout: NextPage = () => {
+const Layout: NextPage = ({ children }) => {
   return (
-    <div className="h-screen w-screen bg-amber-50 dark:bg-zinc-800">
+    <div className="mx-auto h-screen w-screen bg-amber-50 text-zinc-800 dark:bg-zinc-800 dark:text-amber-50">
       <NavigationBar />
+      <main>{children}</main>
     </div>
   );
 };
