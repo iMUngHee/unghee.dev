37f9da762d624b36934ec50fcbd29e9202e76af1
LUH <ung7497@ajou.ac.kr>
2022-03-20 19:11:53 +0900

    [FIX] Add padding top for pc ver. and Set select-none

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,7 +5,9 @@ const Layout: NextPage = ({ children }) => {
   return (
     <div className="min-h-screen w-screen bg-amber-50 text-zinc-800 dark:bg-zinc-800 dark:text-amber-50">
       <NavigationBar />
-      <main className="flex items-center justify-center pt-12">{children}</main>
+      <main className="flex select-none items-center justify-center pt-12 xl:pt-16">
+        {children}
+      </main>
     </div>
   );
 };
