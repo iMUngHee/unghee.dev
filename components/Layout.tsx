772e935235cf1f0656f81a3c5ae87d510dcdd830
LUH <ung7497@ajou.ac.kr>
2022-03-21 17:14:33 +0900

    [FIX] Fix viewport weight

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,9 +3,12 @@ import NavigationBar from "./NavigationBar";
 
 const Layout: NextPage = ({ children }) => {
   return (
-    <div className="min-h-screen w-screen bg-amber-50 text-zinc-800 dark:bg-zinc-800 dark:text-amber-50">
+    <div
+      className="sm:min-w-screen-sm md:min-w-screen-md lg:min-w-screen-lg xl:min-w-screen-xl  min-h-screen
+      bg-amber-50 text-zinc-800 dark:bg-zinc-800 dark:text-amber-50"
+    >
       <NavigationBar />
-      <main className="flex select-none items-center justify-center pt-12 xl:pt-16">
+      <main className="flex w-full select-none items-center justify-center pt-12 xl:pt-16">
         {children}
       </main>
     </div>
