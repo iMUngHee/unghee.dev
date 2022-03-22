caa22064adf8a581ee4ba2815f9675820380e270
LUH <ung7497@ajou.ac.kr>
2022-03-22 17:52:49 +0900

    [POST] Add select category btn

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,16 +1,44 @@
 import Items from "@components/Items";
+import cls from "@libs/cls";
 import { NextPage } from "next";
+import { useState } from "react";
+
+type TPosts = "recent" | "tag";
 
 const Posts: NextPage = () => {
+  const [type, setType] = useState<TPosts>("recent");
+  const onSelect = ({ target: { value } }: any) => {
+    value === "recent" ? setType("recent") : setType("tag");
+  };
   return (
     <div className="mx-auto mt-5 flex w-11/12 flex-col items-center justify-center pb-12">
       <div className="flex w-full flex-row items-center justify-start gap-5">
-        <span className="border-b-2 border-black font-['RocknRoll_One']">
+        <button
+          onClick={onSelect}
+          value="recent"
+          className={cls(
+            "font-['RocknRoll_One']",
+            type === "recent"
+              ? "border-b-2 border-black dark:border-amber-50"
+              : "",
+          )}
+        >
           Recent
-        </span>
-        <span className="font-['RocknRoll_One']">Tag</span>
+        </button>
+        <button
+          value="tag"
+          onClick={onSelect}
+          className={cls(
+            "font-['RocknRoll_One']",
+            type === "recent"
+              ? ""
+              : "border-b-2 border-black dark:border-amber-50",
+          )}
+        >
+          Tag
+        </button>
       </div>
-      <Items />
+      {type === "recent" ? <Items /> : null}
     </div>
   );
 };
