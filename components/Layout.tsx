6572537e66e63f73cd9b215476d7014161ca604d
LUH <ung7497@ajou.ac.kr>
2022-04-03 16:05:03 +0900

    fix; select none -> All pages

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,7 +3,7 @@ import NavigationBar from "./NavigationBar";
 
 const Layout: NextPage = ({ children }) => {
   return (
-    <div className="min-h-screen w-full bg-amber-50 text-zinc-800 dark:bg-zinc-800 dark:text-amber-50">
+    <div className="min-h-screen w-full select-none bg-amber-50 text-zinc-800 dark:bg-zinc-800 dark:text-amber-50">
       <NavigationBar />
       <main className="mx-auto select-none pt-12 [width:calc(100%-2em)] lg:w-[1024px] xl:w-[1376px] xl:pt-16 2xl:w-[1728px]">
         {children}
