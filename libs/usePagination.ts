c4241b06778e84442b28bb73e87e478c336cccb5
LUH <ung7497@ajou.ac.kr>
2022-04-13 00:00:09 +0900

    hooks; Add usePagination

---
+++ libs/usePagination.ts
@@ -0,0 +1,19 @@
+import { PostType } from 'pages/posts';
+import { useState } from 'react';
+
+export default function usePagination(data: PostType[], itemsPerPage: number) {
+  const [currentPage, setCurrentPage] = useState(1);
+  const maxPage = Math.ceil(data.length / itemsPerPage);
+
+  function currentData() {
+    const begin = (currentPage - 1) * itemsPerPage;
+    const end = begin + itemsPerPage;
+    return data.slice(0, end);
+  }
+
+  function next() {
+    setCurrentPage((currentPage) => Math.min(currentPage + 1, maxPage));
+  }
+
+  return { next, currentData, currentPage, maxPage };
+}
