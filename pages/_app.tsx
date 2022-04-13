d2c3c0ecda52d35fd1794e82e20e8ab1f370209f
LUH <ung7497@ajou.ac.kr>
2022-04-13 13:39:51 +0900

    fix; scroll restoration off

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,27 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import { ThemeProvider } from "next-themes";
-import Layout from "@components/Layout";
-import { AnimatePresence } from "framer-motion";
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'next-themes';
+import Layout from '@components/Layout';
+import { AnimatePresence } from 'framer-motion';
+
+if (typeof window !== 'undefined') {
+  window.history.scrollRestoration = 'manual';
+}
 
 function MyApp({ Component, pageProps, router }: AppProps) {
   return (
     <>
       <ThemeProvider attribute="class" enableSystem={true}>
         <Layout>
-          <AnimatePresence exitBeforeEnter initial={true}>
+          <AnimatePresence
+            exitBeforeEnter
+            initial={true}
+            onExitComplete={() => {
+              if (typeof window !== 'undefined') {
+                window.scrollTo({ top: 0 });
+              }
+            }}
+          >
             <Component {...pageProps} key={router.route} />
           </AnimatePresence>
         </Layout>
