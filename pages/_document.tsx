556d83ea291846e95ba16abe898f7a5e69f3d25b
LUH <ung7497@ajou.ac.kr>
2022-05-16 23:05:33 +0900

    [Chore] For safari favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -24,8 +24,28 @@ class MyDocument extends Document {
           <link rel="shortcut icon" href="/favicon.ico" />
           <link
             rel="apple-touch-icon"
+            sizes="60x60"
+            href="/apple-touch-icon-60x60.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="76x76"
+            href="/apple-touch-icon-76x76.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="120x120"
+            href="/apple-touch-icon-120x120.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="152x152"
+            href="/apple-touch-icon-152x152.png"
+          />
+          <link
+            rel="apple-touch-icon"
             sizes="180x180"
-            href="/apple-touch-icon.png"
+            href="/apple-touch-icon-180x180.png"
           />
           <link
             rel="icon"
